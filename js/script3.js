23a1ed02e91c7b07a164e253f4b63d9a4f60e78c
sivakavineshnattudurai <kavinesh342003@gmail.com>
2024-09-13 12:44:48 +0530

    sign-in forgot password added

--- js/script3.js
+++ js/script3.js
@@ -18,7 +18,7 @@ function openrecipes(page) {
     window.location.href = page;
 }
 
-/document.getElementById('reviewForm')?.addEventListener('submit', function(event) {
+/*document.getElementById('reviewForm')?.addEventListener('submit', function(event) {
     event.preventDefault();
 
     const name = document.getElementById('name').value;
@@ -43,4 +43,4 @@ document.getElementById('signinForm').addEventListener('submit', function(event)
         alert('Please use a Gmail address');
         event.preventDefault();
     }
-});
+});/
